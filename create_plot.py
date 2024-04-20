a8435f5bfa42a148574e76ea8b3e90656aa3b5a9
coconoca-8039 <coconoca.8039@gmail.com>
2024-04-20 17:19:22 +0900

    Update: 2024-04-20 17:19:22

--- create_plot.py
+++ create_plot.py
@@ -4,8 +4,16 @@ import matplotlib.pyplot as plt
 
 print("kotoha")
 
+plt.figure(figsize=(10, 5))
+
 x = [1, 2, 3, 4, 5, 6, 7]
 y = [1, 4, 9, 16, 25, 36, 49]
 plt.plot(x,y)
 plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image1.jpg')
+
+x = [1, 2, 3, 4, 5, 6, 7]
+y = np.array(x) ** 2
+plt.plot(x,y)
+plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image2.jpg')
+
 plt.show()
