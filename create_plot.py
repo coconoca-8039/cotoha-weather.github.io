63535dc5cbe5a892b0b960cddecfd86ddadf57a4
coconoca-8039 <coconoca.8039@gmail.com>
2024-04-20 19:49:30 +0900

    Update: 2024-04-20 19:49:30

--- create_plot.py
+++ create_plot.py
@@ -20,7 +20,7 @@ def fetch_recent_data(db_path, table_name, column_name):
 	
 	# 最新データを参照
 	# query = f"SELECT * FROM {table_name} ORDER BY Timestamp DESC LIMIT {limit}"
-	query = f"SELECT {column_name} FROM {table_name} ORDER BY rowid DESC LIMIT 10"
+	query = f"SELECT {column_name} FROM {table_name} ORDER BY rowid DESC LIMIT 72"
 	cursor.execute(query)
 	
 	# 結果をリストに格納
@@ -30,50 +30,67 @@ def fetch_recent_data(db_path, table_name, column_name):
 	for row in cursor.fetchall():
 		if column_name != column_timestamp:
 			data = float(row[0])
-		data_list.append(row[0])
+			data_list.append(data)
+		else:
+			data_list.append(row[0])
 	
 	con.close()
 	
 	return data_list
-
-print("------------")
+	
+plt.figure(figsize=(10, 10))
+plt.rcParams.update({'font.size':10})	
 
 tempture = fetch_recent_data(db_path, table_name, column_tempture)
-print(tempture)
+
+new_timestamp = []
 timestamp = fetch_recent_data(db_path, table_name, column_timestamp)
-print(timestamp)
-print(type(timestamp[0]))
+for date_str in timestamp:
+	# date_str = date_str[5:]
+	date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+	new_timestamp.append(date_obj)
 
-plt.figure(figsize=(10, 10))
-plt.rcParams.update({'font.size':20})
+x = new_timestamp
+y = tempture
+plt.title('temperature')
+plt.plot(x, y)
+plt.xticks(rotation=20)
+plt.xlabel('date')
+plt.ylabel('temperature')
+plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image1.jpg')
+plt.clf()
 
-def test():
-	x = [1, 2, 3, 4, 5, 6, 7]
-	y = [1, 4, 9, 16, 25, 36, 49]
-	plt.title('1')
-	plt.plot(y, x)
-	plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image1.jpg')
-	plt.clf()
 
-	x = [1, 2, 3, 4, 5, 6, 7]
-	y = np.array(x) ** 2
-	plt.title('2')
-	plt.plot(x,y)
-	plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image2.jpg')
-	plt.clf()
+x = new_timestamp
+humidity = fetch_recent_data(db_path, table_name, column_humidity)
+y = humidity
+plt.title('humidity')
+plt.plot(x,y)
+plt.xlabel('date')
+plt.ylabel('humidity')
+plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image2.jpg')
+plt.clf()
 
-	x = [1, 2, 3, 4, 5, 6, 7]
-	y = np.array(x) ** 3
-	plt.title('3')
-	plt.plot(x,y)
-	plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image3.jpg')
-	plt.clf()
 
-	x = [1, 2, 3, 4, 5, 6, 7]
-	y = [7, 6, 5, 4, 3, 2, 1]
-	plt.title('4')
-	plt.plot(x,y)
-	plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image4.jpg')
-	plt.clf()
+x = new_timestamp
+pressure = fetch_recent_data(db_path, table_name, column_pressure)
+y = pressure
+plt.title('pressure')
+plt.plot(x,y)
+plt.xlabel('date')
+plt.ylabel('pressure')
+plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image3.jpg')
+plt.clf()
 
-	# plt.show()
+
+x = new_timestamp
+satellite_count = fetch_recent_data(db_path, table_name, column_satelliteCount)
+y = satellite_count
+plt.title('satellite_count')
+plt.plot(x,y)
+plt.xlabel('date')
+plt.ylabel('satellite_count')
+plt.savefig('/home/pi/Desktop/cotoha/cotoha-weather.github.io/image4.jpg')
+plt.clf()
+
+# plt.show()
