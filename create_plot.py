89af2ed394096f8e0dd99b59416f038447e3ea29
coconoca-8039 <coconoca.8039@gmail.com>
2024-04-20 16:01:02 +0900

    Update: 2024-04-20 16:01:01

--- create_plot.py
+++ create_plot.py
@@ -1,6 +1,10 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 print("kotoha")
 
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+plt.plot(x,y)
+plt.show()
