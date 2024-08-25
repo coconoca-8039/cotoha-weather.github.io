0597b6a62136a920ab74a9598b2943bc69e0aef2
coconoca-8039 <coconoca.8039@gmail.com>
2024-08-25 09:30:28 +0900

    Update: 2024-08-25 09:30:28

--- cpu_temp_log.sh
+++ cpu_temp_log.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 echo "$(date),$(vcgencmd measure_temp)" >> /home/pi/Desktop/cotoha/cotoha-weather.github.io/cpu_temp_log.txt
+
+echo "$(date),$(vcgencmd measure_volts)" >> /home/pi/Desktop/cotoha/cotoha-weather.github.io/volt_log.txt
